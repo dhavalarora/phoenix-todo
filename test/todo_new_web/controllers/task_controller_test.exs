34a528808ace6a500afbd1bd6fe7f237d7402c69
Dhaval Arora <dhaval.arora25@gmail.com>
2021-12-28 14:51:01 +0530

    DRYed task_controller_test

--- test/todo_new_web/controllers/task_controller_test.exs
+++ test/todo_new_web/controllers/task_controller_test.exs
@@ -2,8 +2,6 @@ defmodule TodoNewWeb.TaskControllerTest do
   use TodoNewWeb.ConnCase
 
   import TodoNew.TodoFixtures
-  import TodoNew.AccountsFixtures
-  alias TodoNew.Accounts
 
   @create_attrs %{
     end_time: ~N[2021-12-22 07:01:00],
@@ -25,41 +23,30 @@ defmodule TodoNewWeb.TaskControllerTest do
       |> Map.replace!(:secret_key_base, TodoNewWeb.Endpoint.config(:secret_key_base))
       |> init_test_session(%{})
 
-    %{user: user_fixture(), conn: conn}
+    register_and_log_in_user(%{conn: conn})
   end
 
   describe "index" do
-    test "lists all tasks", %{conn: conn, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
-
+    test "lists all tasks", %{conn: conn} do
       conn = get(conn, Routes.task_path(conn, :index))
       assert html_response(conn, 200) =~ "Your Tasks"
     end
   end
 
   describe "new task" do
-    test "renders form", %{conn: conn, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
-
+    test "renders form", %{conn: conn} do
       conn = get(conn, Routes.task_path(conn, :new))
       assert html_response(conn, 200) =~ "New Task"
     end
   end
 
   describe "create task" do
-    test "redirects to index when data is valid", %{conn: conn, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
-
+    test "redirects to index when data is valid", %{conn: conn} do
       conn = post(conn, Routes.task_path(conn, :create), task: @create_attrs)
       assert redirected_to(conn) == Routes.task_path(conn, :index)
     end
 
-    test "renders errors when data is invalid", %{conn: conn, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
+    test "renders errors when data is invalid", %{conn: conn} do
       conn = post(conn, Routes.task_path(conn, :create), task: @invalid_attrs)
       assert html_response(conn, 200) =~ "New Task"
     end
@@ -69,11 +56,7 @@ defmodule TodoNewWeb.TaskControllerTest do
     setup [:create_task]
 
     test "redirects when requested by another user", %{conn: conn, task: task} do
-      new_user = user_fixture()
-
-      token = Accounts.generate_user_session_token(new_user)
-      conn = put_session(conn, :user_token, token)
-
+      %{conn: conn, user: _new_user} = register_and_log_in_user(%{conn: conn})
       conn = get(conn, Routes.task_path(conn, :show, task))
       assert redirected_to(conn) == Routes.task_path(conn, :index)
     end
@@ -82,19 +65,13 @@ defmodule TodoNewWeb.TaskControllerTest do
   describe "edit task" do
     setup [:create_task]
 
-    test "renders form for editing chosen task", %{conn: conn, task: task, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
+    test "renders form for editing chosen task", %{conn: conn, task: task} do
       conn = get(conn, Routes.task_path(conn, :edit, task))
       assert html_response(conn, 200) =~ "Edit Task"
     end
 
     test "redirect when editing with different user", %{conn: conn, task: task} do
-      new_user = user_fixture()
-
-      token = Accounts.generate_user_session_token(new_user)
-      conn = put_session(conn, :user_token, token)
-
+      %{conn: conn, user: _new_user} = register_and_log_in_user(%{conn: conn})
       conn = get(conn, Routes.task_path(conn, :edit, task))
       assert redirected_to(conn) == Routes.task_path(conn, :index)
     end
@@ -103,10 +80,7 @@ defmodule TodoNewWeb.TaskControllerTest do
   describe "update task" do
     setup [:create_task]
 
-    test "redirects when data is valid", %{conn: conn, task: task, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
-
+    test "redirects when data is valid", %{conn: conn, task: task} do
       conn = put(conn, Routes.task_path(conn, :update, task), task: @update_attrs)
       assert redirected_to(conn) == Routes.task_path(conn, :show, task)
 
@@ -114,19 +88,13 @@ defmodule TodoNewWeb.TaskControllerTest do
       assert html_response(conn, 200) =~ "some updated title"
     end
 
-    test "renders errors when data is invalid", %{conn: conn, task: task, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
+    test "renders errors when data is invalid", %{conn: conn, task: task} do
       conn = put(conn, Routes.task_path(conn, :update, task), task: @invalid_attrs)
       assert html_response(conn, 200) =~ "Edit Task"
     end
 
     test "redirects when updating with different user", %{conn: conn, task: task} do
-      new_user = user_fixture()
-
-      token = Accounts.generate_user_session_token(new_user)
-      conn = put_session(conn, :user_token, token)
-
+      %{conn: conn, user: _new_user} = register_and_log_in_user(%{conn: conn})
       conn = put(conn, Routes.task_path(conn, :update, task), task: @update_attrs)
       assert redirected_to(conn) == Routes.task_path(conn, :index)
     end
@@ -135,9 +103,7 @@ defmodule TodoNewWeb.TaskControllerTest do
   describe "delete task" do
     setup [:create_task]
 
-    test "deletes chosen task", %{conn: conn, task: task, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
+    test "deletes chosen task", %{conn: conn, task: task} do
       conn = delete(conn, Routes.task_path(conn, :delete, task))
       assert redirected_to(conn) == Routes.task_path(conn, :index)
 
@@ -147,11 +113,7 @@ defmodule TodoNewWeb.TaskControllerTest do
     end
 
     test "redirects when deleting with different user", %{conn: conn, task: task} do
-      new_user = user_fixture()
-
-      token = Accounts.generate_user_session_token(new_user)
-      conn = put_session(conn, :user_token, token)
-
+      %{conn: conn, user: _new_user} = register_and_log_in_user(%{conn: conn})
       conn = delete(conn, Routes.task_path(conn, :delete, task))
       assert redirected_to(conn) == Routes.task_path(conn, :index)
     end
@@ -160,11 +122,8 @@ defmodule TodoNewWeb.TaskControllerTest do
   describe "update status" do
     setup [:create_task]
 
-    test "updates status from New -> Working -> Completed", %{conn: conn, task: task, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
+    test "updates status from New -> Working -> Completed", %{conn: conn, task: task} do
       assert task.status == :New
-
       conn = patch(conn, Routes.task_path(conn, :update_status, task.id), new_status: "Working")
       conn = get(conn, Routes.task_path(conn, :show, task))
       assert html_response(conn, 200) =~ "Working"
@@ -174,11 +133,11 @@ defmodule TodoNewWeb.TaskControllerTest do
       assert html_response(conn, 200) =~ "Completed"
     end
 
-    test "no update when new status is not acc. to flow New -> Working -> Completed", %{conn: conn, task: task, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
+    test "no update when new status is not acc. to flow New -> Working -> Completed", %{
+      conn: conn,
+      task: task
+    } do
       assert task.status == :New
-
       conn = patch(conn, Routes.task_path(conn, :update_status, task.id), new_status: "Completed")
       conn = get(conn, Routes.task_path(conn, :show, task))
       assert html_response(conn, 200) =~ "New"
@@ -201,11 +160,7 @@ defmodule TodoNewWeb.TaskControllerTest do
     end
 
     test "redirects when updating with different user", %{conn: conn, task: task} do
-      new_user = user_fixture()
-
-      token = Accounts.generate_user_session_token(new_user)
-      conn = put_session(conn, :user_token, token)
-
+      %{conn: conn, user: _new_user} = register_and_log_in_user(%{conn: conn})
       conn = patch(conn, Routes.task_path(conn, :update_status, task.id), new_status: "Working")
       assert redirected_to(conn) == Routes.task_path(conn, :index)
     end
@@ -215,27 +170,32 @@ defmodule TodoNewWeb.TaskControllerTest do
     setup [:create_task]
 
     test "Mark completed tasks as deleted", %{conn: conn, task: task, user: user} do
-      token = Accounts.generate_user_session_token(user)
-      conn = put_session(conn, :user_token, token)
       assert task.status == :New
-
       conn = patch(conn, Routes.task_path(conn, :update_status, task.id, new_status: "Working"))
-      conn = get(conn, Routes.task_path(conn, :show, task))
-      assert html_response(conn, 200) =~ "Working"
-
       conn = patch(conn, Routes.task_path(conn, :update_status, task.id), new_status: "Completed")
       conn = get(conn, Routes.task_path(conn, :show, task))
+      assert html_response(conn, 200) =~ "Completed"
+
+      task2 = task_fixture(%{user_id: user.id})
+      assert task2.status == :New
+      conn = patch(conn, Routes.task_path(conn, :update_status, task2.id, new_status: "Working"))
+
+      conn =
+        patch(conn, Routes.task_path(conn, :update_status, task2.id), new_status: "Completed")
+
+      conn = get(conn, Routes.task_path(conn, :show, task2))
       assert html_response(conn, 200) =~ "Completed"
 
       conn = get(conn, Routes.task_path(conn, :clear_completed))
       conn = get(conn, Routes.task_path(conn, :show, task))
       assert html_response(conn, 200) =~ "Deleted"
+      conn = get(conn, Routes.task_path(conn, :show, task2))
+      assert html_response(conn, 200) =~ "Deleted"
     end
   end
 
-  defp create_task(_) do
-    user = user_fixture()
+  defp create_task(%{conn: _conn, user: user}) do
     task = task_fixture(%{user_id: user.id})
-    %{user: user, task: task}
+    %{task: task}
   end
 end
